6622c27a85c8e3b33de4ea4ca432f4c9b9096d34
MARIANA KOVAL <marianakoval@MacBook-Pro-MARIANA.local>
2022-04-12 15:59:06 +0300

    arrays with moves

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 "use strict";
 
-const cells = document.querySelectorAll('.cell');
+const movesX = [];
+const movesO = [];
+const winnerMovew = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const cell = document.querySelectorAll('.cell');
 
 function moves() {
     let i = 0;
-cells.forEach(item => {
+cell.forEach(item => {
     item.addEventListener('click', event => {
         if (i % 2 == 0) {
         item.innerHTML = 'X';
+        movesX.push(item.dataset.value);
         } else {
             item.innerHTML = '0';
+            movesO.push(item.dataset.value);
         }
         i++;
+        return (movesX, movesO);
     }, {once: true});
+    
     });
 }
   
